2acfe3415bf51a2f08401d249c4de473a425c5f8
Michael He <hlxwell@gmail.com>
2011-10-07 00:15:59 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ISafePlayerHomePage::Application.config.secret_token = 'e0457790ba674f2562ae13e50da3852a891816b2d417601d99da07a7d3d136a6db013a931500ae81fa6c30d53807628d4a0ed10eac6d2fd199b765f0ccfd1330'
