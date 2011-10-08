2f6482304029ad7dd38b7f720a4971f6c1ef17b8
Michael He <hlxwell@gmail.com>
2011-10-08 10:32:51 +0800

    add localization file add unicorn config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,24 @@
 module ApplicationHelper
+
+  # <li>
+  #   Home
+  # </li>
+  # <li>
+  #   <a href='/'>Feedback</a>
+  # </li>
+  # <li class='no_right'>
+  #   <a href='/'>About</a>
+  # </li>
+  #
   def top_menu
-    # <li>
-    #   Home
-    # </li>
-    # <li>
-    #   <a href='/'>Feedback</a>
-    # </li>
-    # <li class='no_right'>
-    #   <a href='/'>About</a>
-    # </li>
-    
     output_html = ""
     [
       ["Home", root_path],
-      ["Feedback", feedbacks_path, true]
-      # ["About", about_path, true] 
+      ["Feedback", feedbacks_path],
+      I18n.locale == :zh ? ["English", "?locale=en", true] : ["中文", "?locale=zh", true]
     ].each do |title, path, is_last_one|
       options = is_last_one ? {:class => 'no_right'} : {}
-      content = current_page?(path) ? title : content_tag(:a, title, :href => path)      
+      content = current_page?(path) ? title : content_tag(:a, title, :href => path)
       output_html << content_tag(:li, content, options)
     end
     output_html
